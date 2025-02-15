578f12328d5b2a4bdb172c01e35ae65e79034261
kobold-king <nick.ragona@gmail.com>
2025-02-14 19:05:38 -0700

    player class

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
@@ -13,6 +13,8 @@ def main():
     #setting up the fps/delta time
     clock = pygame.time.Clock()
     dt = 0
+    #define Player
+    player =  Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting asteroids!")
     print(f"Screen width:", (SCREEN_WIDTH))
@@ -21,6 +23,7 @@ def main():
     while True:
         #draws the screen and updates
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
         #This checks to see if the close button has been pressed
